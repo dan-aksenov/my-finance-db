6647e9b484b6a3f2fcb1bcfd4c46231b33747454
dbax <dan-aksenov@yandex.ru>
2017-03-06 23:11:31 +0300

    barber script added to replace dict values with number

---
+++ barber.sh
@@ -0,0 +1,4 @@
+sed -i 's/\tзарплатная\ карта\t/\t4\t/' test_trn16.sql
+sed -i 's/\tдебетная\ карта\t/\t3\t/' test_trn16.sql
+sed -i 's/\tкредитная\ карта\t/\t2\t/' test_trn16.sql
+sed -i 's/\tналичные\t/\t1\t/' test_trn16.sql
