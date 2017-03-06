d5d6df7c04a416875806ed3823892e048462a65f
dbax <dan-aksenov@yandex.ru>
2017-03-06 23:19:56 +0300

    more work on barber

--- barber.sh
+++ barber.sh
@@ -2,3 +2,28 @@ sed -i 's/\tзарплатная\ карта\t/\t4\t/' test_trn16.sql
 sed -i 's/\tдебетная\ карта\t/\t3\t/' test_trn16.sql
 sed -i 's/\tкредитная\ карта\t/\t2\t/' test_trn16.sql
 sed -i 's/\tналичные\t/\t1\t/' test_trn16.sql
+
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\t\t/\t\t/' test_trn16.sql
