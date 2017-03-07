f766dc5146d0fc6c08a6178bd1c3f6807dd88702
dbax <dan-aksenov@yandex.ru>
2017-03-07 23:35:19 +0300

    barber script completed

--- barber.sh
+++ barber.sh
@@ -3,27 +3,27 @@ sed -i 's/\tдебетная\ карта\t/\t3\t/' test_trn16.sql
 sed -i 's/\tкредитная\ карта\t/\t2\t/' test_trn16.sql
 sed -i 's/\tналичные\t/\t1\t/' test_trn16.sql
 
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
-sed -i 's/\t\t/\t\t/' test_trn16.sql
+sed -i 's/\tАвтомобиль\t/\t1\t/' test_trn16.sql
+sed -i 's/\tПродукты\t/\t2\t/' test_trn16.sql
+sed -i 's/\tРазвлечения\t/\t3\t/' test_trn16.sql
+sed -i 's/\tКафе и рестораны\t/\t4\t/' test_trn16.sql
+sed -i 's/\tОбед на работе\t/\t5\t/' test_trn16.sql
+sed -i 's/\tИнвестиции\t/\t6\t/' test_trn16.sql
+sed -i 's/\tБензин\t/\t7\t/' test_trn16.sql
+sed -i 's/\tУчёба\t/\t8\t/' test_trn16.sql
+sed -i 's/\tПромтовары\t/\t9\t/' test_trn16.sql
+sed -i 's/\tПодарки\t/\t10\t/' test_trn16.sql
+sed -i 's/\tКвартплата\t/\t11\t/' test_trn16.sql
+sed -i 's/\tБанк.комиссия\t/\t12\t/' test_trn16.sql
+sed -i 's/\tМобильник\t/\t13\t/' test_trn16.sql
+sed -i 's/\tИнтернет\t/\t14\t/' test_trn16.sql
+sed -i 's/\tПарковка\t/\t15\t/' test_trn16.sql
+sed -i 's/\tМедицина\t/\t16\t/' test_trn16.sql
+sed -i 's/\tТранспорт\t/\t17\t/' test_trn16.sql
+sed -i 's/\tУслуги\t/\t18\t/' test_trn16.sql
+sed -i 's/\tсамолет\t/\t19\t/' test_trn16.sql
+sed -i 's/\tГостиница\t/\t20\t/' test_trn16.sql
+sed -i 's/\tПоезд\t/\t21\t/' test_trn16.sql
+sed -i 's/\tДругое\t/\t22\t/' test_trn16.sql
+sed -i 's/\tСпорт\t/\t23\t/' test_trn16.sql
+sed -i 's/\tЧаевые\t/\t24\t/' test_trn16.sql
